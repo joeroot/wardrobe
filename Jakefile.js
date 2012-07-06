1bb885b4fb7f312f85386c3ec32e913ff616d6ba
Joe Root <joe@onlysix.co.uk>
2012-07-06 23:34:49 +0100

    Jakefile for creating browser version.

---
+++ Jakefile.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+
+desc('Builds Wardrobe for the browser.');
+task('browser', function (params) {
+  var files = ['lexer', 'parser', 'interpreter', 'wardrobe'];
+  var src = '';
+ 
+  src = src + "(function(root) {\n"
+  src = src + "  var Wardrobe = function() {\n"
+  src = src + "    function require(path){ return require[path]; }"
+  
+  for (i = 0; i < files.length; i++) {
+    var file = files[i];
+    src = src + "require['./" + file + "'] = new function() {\n";
+    src = src + "  var exports = this;\n";
+    src = src + "  " + fs.readFileSync("src/" + file + ".js");
+    src = src + "}\n";
+  }
+
+  src = src + "    return require['./wardrobe'];"
+  src = src + "  }();"
+  src = src + "    if (typeof define === 'function' && define.amd) {"
+  src = src + "      define(function() { return Wardrobe; });"
+  src = src + "    } else { "
+  src = src + "     root.Wardrobe = Wardrobe;" 
+  src = src + "     }"
+  src = src + "  }(this));"
+
+  fs.writeFile('lib/wardrobe.js', src);
+});
